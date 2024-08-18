311d66f6762c220ed649829a9ee1569cca14fdac
JerryMoo <wuchujie123@gmail.com>
2024-08-18 23:43:40 +0800

    update: update options

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,4 +2,12 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+-- General Settiong
 vim.opt.mouse = ""
+
+-- Editor UI
+vim.opt.wrap = true
+vim.opt.colorcolumn = "120" -- Column highlight at textwidth's max character-limit
+
+-- Tabs And Indents
+vim.opt.textwidth = 100
