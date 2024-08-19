b9319974a66d7e806f923fc92f96029981b52821
JerryMoo <wuchujie123@gmail.com>
2024-08-20 01:00:14 +0800

    update: update options

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -4,9 +4,14 @@
 
 -- General Settiong
 vim.opt.mouse = ""
+vim.opt.winbar = "%=%m %f"
+
+-- Vim Directories
+vim.opt.swapfile = false
 
 -- Editor UI
 vim.opt.wrap = true
+vim.opt.ruler = true
 vim.opt.colorcolumn = "120" -- Column highlight at textwidth's max character-limit
 
 -- Tabs And Indents
