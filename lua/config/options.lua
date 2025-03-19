a978f64709d957822da64e0f861e52e902930fe4
JerryMoo <wuchujie123@gmail.com>
2025-03-20 01:17:43 +0800

    update options.lua

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,18 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- General Setting
+vim.opt.mouse = ""
+vim.opt.winbar = "%=%m %f"
+
+-- Vim Directories
+vim.opt.swapfile = false
+
+-- Editor UI
+vim.opt.wrap = true
+vim.opt.ruler = true
+vim.opt.colorcolumn = "120" -- Column highlight at textwidth's max character-limit
+
+-- Tabs And Indents
+vim.opt.textwidth = 100
