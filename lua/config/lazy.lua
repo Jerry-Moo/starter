195dd68a6a3965cc89d777ce5dd289978fba363c
JerryMoo <wuchujie123@gmail.com>
2025-03-22 17:10:06 +0800

    update

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -18,6 +18,7 @@ require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+    { import = "lazyvim.plugins.extras.editor.neo-tree" },
     -- language
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.json" },
